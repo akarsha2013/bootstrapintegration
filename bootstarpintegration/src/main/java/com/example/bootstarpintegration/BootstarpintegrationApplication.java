648c89e25b90ec4c644e65f31d8c637e46a4aa73
akarsha2013 <akarshaaku6@gmail.com>
2025-09-06 19:58:58 +0530

    Add files via upload

---
+++ bootstarpintegration/src/main/java/com/example/bootstarpintegration/BootstarpintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstarpintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstarpintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstarpintegrationApplication.class, args);
+	}
+
+}
